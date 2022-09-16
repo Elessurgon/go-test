2da220adc759270f45f68837dd94bf3ce2e826d1
Elesssurgon <LancelotSycorax@gmail.com>
2022-09-16 16:24:20 +0530

    Added some deadlock prevention

--- db/query/account.sql
+++ db/query/account.sql
@@ -1,4 +1,4 @@
--- name: CreateAuthor :one
+-- name: CreateAccount :one
 INSERT INTO accounts (
     owner,
     balance,
@@ -6,3 +6,20 @@ INSERT INTO accounts (
 ) VALUES (
     $1, $2, $3
 ) RETURNING *;
+
+-- name: GetAaccount :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1;
+
+-- name: ListAccounts :many
+SELECT * FROM accounts
+ORDER BY id
+LIMIT $1
+OFFSET $2;
+
+-- name: UpdateAccount :exec
+UPDATE accounts SET balance = $2 
+WHERE id = $1;
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts WHERE id = $1;
